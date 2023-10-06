b04e2be7b79d156c8d9a952a78f66f42ad90cb1f
danygutmann <mail@danielgutmann.de>
2023-10-06 12:46:36 +0200

    last

--- ATmega88_firstTest/main.asm
+++ ATmega88_firstTest/main.asm
@@ -1,10 +1,10 @@
 
 
 
-#define Line1_Led_alarm PC5			;led für alarm
-#define Line1_Led_ok PC4			;led für alles ist gut
-#define Line1_Sensor PD7
-#define System_reset PC6
+#define Line1_Led_alarm PC5			; led für alarm
+#define Line1_Led_ok PC4			; led für alles ist gut
+#define Line1_Sensor PD7			; eingang alarm
+#define System_reset PD6			; eingang reset
 
 init:
 	; Ausgänge - der rest ist automatisch ein Eingang   
@@ -12,27 +12,43 @@ init:
     out DDRC, r16     
 	
 	; setzen der internen pull-up widerstände
-	ldi r16, 1<<Line1_Sensor 
-	out PORTD, r16        
-
-	rjmp startup
+	ldi r16, 1<<Line1_Sensor | 1<<System_reset 
+	out PORTD, r16    
+	rjmp startupAndReset
 
 loop:
-	sbi PORTC, Line1_Led_ok			;Alles ok an
-	cbi PORTC, Line1_Led_alarm		;Alarm LED aus
-	sbis PIND, Line1_Sensor
-	rcall alarm
-
+	sbis PIND, Line1_Sensor			; wenn sensor high
+	rcall loop_alarm				; springe in Alarmschleife
 	rjmp loop
 
-startup:
-	sbi PORTC, Line1_Led_ok			;Alles ok an
-	cbi PORTC, Line1_Led_alarm		;Alarm LED aus
 
+loop_alarm:							; wird im alarmfall aufgerufen
+	cbi PORTC, Line1_Led_ok			; Alles ok aus
+	sbi PORTC, Line1_Led_alarm		; Alarmleuchte an
+	rcall timer						; pause
+	cbi PORTC, Line1_Led_alarm		; alarm blink aus
+	rcall timer						; pause
+
+	sbis PIND, System_reset			; wenn reset high
+	rjmp startupAndReset			; springe zum Reset
+
+	rjmp loop_alarm
+
+startupAndReset:
+	sbi PORTC, Line1_Led_ok			; Alles ok an
+	cbi PORTC, Line1_Led_alarm		; Alarm LED aus
 	rjmp loop
 
-; wird im alarmfall aufgerufen
-alarm:
-	cbi PORTC, Line1_Led_ok			;Alles ok aus
-	sbi PORTC, Line1_Led_alarm		;Alarmleuchte an
-	rjmp loop
+timer:
+    ldi r16,0                   
+    ldi r17,0
+    ldi r18,1
+
+timer2:
+    inc r16                    
+    brne timer2					
+    inc r17                    
+    brne timer2					
+    dec r18	
+    brne timer2
+    ret   
